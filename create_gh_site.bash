6da5e0a448de479db15f356c0e4606640e336cf8
Robert L. Read <read.robert@gmail.com>
2014-08-27 17:44:09 -0400

    making more generic

--- create_gh_site.bash
+++ create_gh_site.bash
@@ -4,23 +4,28 @@ export REPONAME=$1
 export PERSONAL_NAME=$2
 export GH_USER=$3
 export GH_PASSWORD=$4
+export GIT_URL_TO_CLONE=$5
+export GIT_ORGANIZATION=$6
+export GIT_REPONAME_TO_CLONE=$7
 
-curl --user "$GH_USER:$GH_PASSWORD" --request POST --data '{"name" : "'$REPONAME'", "description": "This is a repository for '$PERSONAL_NAME'",  "homepage": "https://someurlThatAlanWillCreate",  "private": false,  "has_issues": true,  "has_wiki": true,  "has_downloads": true}' https://api.github.com/orgs/MyInternetWebsite/repos
+echo 'https://api.github.com/orgs/'$GIT_ORGAINZATION'/repos'
+
+curl --user "$GH_USER:$GH_PASSWORD" --request POST --data '{"name" : "'$REPONAME'", "description": "This is a repository for '$PERSONAL_NAME'",   "private": false,  "has_issues": true,  "has_wiki": true,  "has_downloads": true}' 'https://api.github.com/orgs/'$GIT_ORGANIZATION'/repos'
 
 mkdir testpush
 cd testpush
 
 
-git clone https://github.com/MyInternetWebsite/microsite-template0.git
-cd microsite-template0
+git clone $GIT_URL_TO_CLONE
+cd $GIT_REPONAME_TO_CLONE
 git checkout gh-pages
 cat index.html | sed 's/663c8bbc10b66148ed44763d59a2ee4e/'$PERSONAL_NAME'/' > index.html.x
 mv index.html.x index.html
 git add index.html
 git commit -m "First pages commit"
-git remote set-url origin https://github.com/MyInternetWebsite/$REPONAME.git
+git remote set-url origin https://github.com/$GIT_ORGANIZATION/$REPONAME.git
 git push origin gh-pages
 cd ..
-rm -rf microsite-template0
+rm -rf $GIT_REPONAME_TO_CLONE
 cd ..
 rm -rf testpush
