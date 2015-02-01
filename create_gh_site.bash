94a4d0b4d1854f066fe1bb827ba5b06e301d9cac
vzvenyach <vdavez@gmail.com>
2015-02-01 16:04:45 -0500

    added .env option rather than requiring it at the command line

--- create_gh_site.bash
+++ create_gh_site.bash
@@ -1,20 +1,19 @@
 #!/bin/bash
-echo "Creating reponame: $1 for a cool person named: $2 using github user: $3 with a github password passed as the fourth argument"
-export REPONAME=$1
-export PERSONAL_NAME=$2
-export GH_USER=$3
-export GH_PASSWORD=$4
-export GIT_URL_TO_CLONE=$5
-export GIT_ORGANIZATION=$6
-export GIT_REPONAME_TO_CLONE=$7
 
-echo 'https://api.github.com/orgs/'$GIT_ORGAINZATION'/repos'
+filename=".env"
+while read -r line
+do
+    name=$line
+    export $name
+done < "$filename"
+
+echo "Creating Repo"
+echo 'https://api.github.com/orgs/'$GIT_ORGANIZATION'/repos'
 
 curl --user "$GH_USER:$GH_PASSWORD" --request POST --data '{"name" : "'$REPONAME'", "description": "This is a repository for '$PERSONAL_NAME'",   "private": false,  "has_issues": true,  "has_wiki": true,  "has_downloads": true}' 'https://api.github.com/orgs/'$GIT_ORGANIZATION'/repos'
 
 mkdir testpush
 cd testpush
-
 
 git clone $GIT_URL_TO_CLONE
 cd $GIT_REPONAME_TO_CLONE
